d6bf36720e5923689621a3b55e0d4acd26a55b5d
brijesh452021 <81951806+brijesh452021@users.noreply.github.com>
2021-04-05 20:51:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,48 +21,40 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-  if(request.method=='POST'):
-    Present_Price=float(request.form['Present_Price'])
-    Kms_Driven=request.form['Kms_Driven'];
-    Kms_Driven2=np.log(Kms_Driven)
-    Owner=request.form['Owner']
-    Number_of_years=int(2020-request.form['Year'])
-    Fuel_Type=request.form['Fuel_Type_Petrol']
-    if(Fuel_Type=='Petrol'):
-      Fuel_Type_Diesel=0
-      Fuel_Type_Petrol=1
-    elif(Fuel_Type=='Diesel'):
-      Fuel_Type_Diesel=1
-      Fuel_Type_Petrol=0
+  def predict():
+    Fuel_Type_Diesel=0
+    if request.method == 'POST':
+        Year = int(request.form['Year'])
+        Present_Price=float(request.form['Present_Price'])
+        Kms_Driven=int(request.form['Kms_Driven'])
+        Kms_Driven2=np.log(Kms_Driven)
+        Owner=int(request.form['Owner'])
+        Fuel_Type_Petrol=request.form['Fuel_Type_Petrol']
+        if(Fuel_Type_Petrol=='Petrol'):
+                Fuel_Type_Petrol=1
+                Fuel_Type_Diesel=0
+        else:
+            Fuel_Type_Petrol=0
+            Fuel_Type_Diesel=1
+        Year=2020-Year
+        Seller_Type_Individual=request.form['Seller_Type_Individual']
+        if(Seller_Type_Individual=='Individual'):
+            Seller_Type_Individual=1
+        else:
+            Seller_Type_Individual=0	
+        Transmission_Mannual=request.form['Transmission_Mannual']
+        if(Transmission_Mannual=='Mannual'):
+            Transmission_Mannual=1
+        else:
+            Transmission_Mannual=0
+        prediction=model.predict([[Present_Price,Kms_Driven2,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
+        output=round(prediction[0],2)
+        if output<0:
+            return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
+        else:
+            return render_template('index.html',prediction_text="You Can Sell The Car at {}".format(output))
     else:
-      Fuel_Type_Diesel=0
-      Fuel_Type_Petrol=0
-
-    Seller_Type=request.form['Seller_Type_Individual']
-
-    if(Seller_Type=='Dealer'):
-      Seller_Type_Individual=0
-    else:
-      Seller_Type_Individual=1
-
-    Transmission=request.form['Transmission_Mannual']
-
-    if(Transmission=='Mannual'):
-      Transmission_Manual=1
-    else:
-      Transmission_Manual=0
-
-    prediction=CarDekhoPrediction.predict([[Present_Price,Kms_Driven2,Owner,
-        Number_of_years,Fuel_Type_Diesel,Fuel_Type_Petrol,
-        Seller_Type_Individual,Transmission_Manual]])
-    output=round(prediction[0],2)
-    if(output>0):
-      return render_template('index.html',prediction_text="Selling Price: {}".format(output))
-    else:
-      return render_template('index.html',prediction_text="Cant Sell")
-
-  else:
-    return render_template('index.html')
+        return render_template('index.html')
 
 if(__name__=="__main__"):
   app.run(debug=True)
